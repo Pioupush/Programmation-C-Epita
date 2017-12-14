62b6652958b0a442c3b61b1dfc0b9b4f10e05649
Pioupush <34543365+Pioupush@users.noreply.github.com>
2017-12-14 17:12:13 +0100

    pokemon class

--- pokemon/Skeleton_v2/skeleton/miniPokemon/miniPokemon/Pokemon.cs
+++ pokemon/Skeleton_v2/skeleton/miniPokemon/miniPokemon/Pokemon.cs
@@ -17,58 +17,77 @@ namespace miniPokemon
 
         #region Constructor
 
-        
-        public Pokemon(string name, int life, int damage, Poketype poketype)
-        : base(name)
+        private int life;
+        private int damage;
+        private Poketype poketype;
+        private int level = 1;
+        private bool isKO = false;
+
+        public int Life
         {
-            this.Name = name;
+            get { return life; }
+            set { life = value; }
+        }
+
+        public bool IsKO
+        {
+            get { return isKO; }
+            set { isKO = value; }
+        }
+
+
+        public Pokemon(string name, int life, int damage, Poketype poketype)
+            : base(name)
+        {
             this.life = life;
-            
+            this.damage = damage;
+            this.poketype = poketype;
         }
 
         #endregion Constructor
-        
+
 
         #region Methods
 
-        
         public override void WhoAmI()
         {
-            // TODO
-            throw new NotImplementedException("Please fix this quickly");
+            Console.WriteLine("I'm a Pokemon");
         }
 
         public override void Describe()
         {
-            // TODO
-            throw new NotImplementedException("Please fix this quickly");
+            Console.Write("My name is " + Name + " I'm a Pokemon of type ");
+            Console.WriteLine(poketype + " and I'm level " + level);
         }
-        
+
         public void LevelUp()
         {
-            // TODO
-            throw new NotImplementedException("Please fix this quickly");
+            ++level;
         }
 
         public int Attack()
         {
-            // TODO
-            throw new NotImplementedException("Please fix this quickly");
+            Console.WriteLine(Name + " uses cut, it's super effective");
+            return damage;
         }
 
         public void GetHurt(int damage)
         {
-            // TODO
-            throw new NotImplementedException("Please fix this quickly");
+            life = life - damage;
+            if (life < 0)
+            {
+                life = 0;
+                isKO = true;
+            }
         }
 
-        public void Heal(int life)
+        public void Heal(int life) 
         {
-            // TODO
-            throw new NotImplementedException("Please fix this quickly");
+            Life = life + Life;
+            if (Life > 0)
+                isKO = false;
         }
 
         #endregion Methods
-        
     }
 }
